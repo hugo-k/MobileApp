ae166e277115753036e743cea90118d05d7fb834
louis <louis.lagrange@outlook.fr>
2023-10-29 19:37:59 +0100

    Done : Go back button on Data_display activity is working and changing theme disabled

--- app/src/main/java/com/example/mobileapp/Data_display.java
+++ app/src/main/java/com/example/mobileapp/Data_display.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
 import android.content.res.Resources;
+import android.view.View;
+import android.widget.ImageButton;
+
 import java.io.InputStream;
 
 public class Data_display extends AppCompatActivity {
@@ -13,6 +16,16 @@ public class Data_display extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_display);
+
+        ImageButton goToMapsActivity = (ImageButton)findViewById(R.id.goToMapsActivity);
+        goToMapsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: See if it's better to use stack or finish activity
+                finish();
+            }
+        });
+
     }
 
 
