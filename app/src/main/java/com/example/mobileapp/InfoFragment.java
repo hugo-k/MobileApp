d04648f0f36f89907ec7768e5d2daa0e91d25942
Hugo <hugo.kantardjian@edu.esiee.fr>
2023-11-01 10:54:36 +0100

    Remove button test and add button in fragment to open data display activity

--- app/src/main/java/com/example/mobileapp/InfoFragment.java
+++ app/src/main/java/com/example/mobileapp/InfoFragment.java
@@ -1,5 +1,6 @@
 package com.example.mobileapp;
 
+import android.content.Intent;
 import android.location.Address;
 import android.os.Bundle;
 
@@ -9,10 +10,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class InfoFragment extends Fragment{
+public class InfoFragment extends Fragment {
 
     private TextView addressTextView;
 
@@ -24,13 +26,21 @@ public class InfoFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_info, container, false);
         addressTextView = view.findViewById(R.id.txtAddress);
 
+        Button openInformations = view.findViewById(R.id.btnDirection);
+        openInformations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), Data_display.class));
+            }
+        });
+
         return view;
     }
 
     public void updatePostalAddress(String addressText) {
         if (addressTextView != null) {
+            Log.d("GetAddress", addressText);
             addressTextView.setText(addressText);
-            Log.d("address", addressText);
         }
     }
 
