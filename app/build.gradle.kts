3f4e5242743024a9ae520c7219a2b8e26c974080
Hugo <hugo.kantardjian@edu.esiee.fr>
2023-11-05 19:10:45 +0100

    Some changes in info fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("com.github.kaelaela:InfiniteViewPager:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/mobileapp/InfoFragment.java
+++ app/src/main/java/com/example/mobileapp/InfoFragment.java
@@ -2,6 +2,7 @@ package com.example.mobileapp;
 
 import android.content.Intent;
 import android.location.Address;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -12,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,10 +24,11 @@ public class InfoFragment extends Fragment {
     private Marker associatedMarker;
     public int fragmentId;
 
-    public static InfoFragment newInstance(String address) {
+    public static InfoFragment newInstance(String address, String wasteType) {
         InfoFragment fragment = new InfoFragment();
         Bundle args = new Bundle();
         args.putString("address", address);
+        args.putString("wasteType", wasteType);
         fragment.setArguments(args);
         return fragment;
     }
@@ -34,18 +37,29 @@ public class InfoFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         fragmentId = getArguments().getInt("fragmentId", -1);
-        Log.d("fragmentId", ""+ fragmentId);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_info, container, false);
         TextView addressTextView = view.findViewById(R.id.txtAddress);
+        TextView wasteTypeTxtView = view.findViewById(R.id.wasteTypeTxtView);
+        ImageButton btnDataDisplay = view.findViewById(R.id.btnMoreInfo);
+
+        btnDataDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         Bundle args = getArguments();
         if (args != null) {
             String address = args.getString("address");
             addressTextView.setText(address);
+
+            String wasteType = args.getString("wasteType");
+            wasteTypeTxtView.setText(wasteType);
         }
         return view;
     }
