95d1d794a5c41087a0993e01ca3bccf5e41898c7
Hugo <hugo.kantardjian@edu.esiee.fr>
2023-11-10 01:04:07 +0100

    test commit before checkout other branch

--- app/src/main/java/com/example/mobileapp/InfoFragment.java
+++ app/src/main/java/com/example/mobileapp/InfoFragment.java
@@ -21,68 +21,46 @@ import com.google.android.gms.maps.model.Marker;
 
 public class InfoFragment extends Fragment {
 
-    private TextView txtAddress;
-    public int fragmentId;
+    private TextView addressTextView;
 
-    public static InfoFragment newInstance(String address, String wasteType) {
-        InfoFragment fragment = new InfoFragment();
-        Bundle args = new Bundle();
-        args.putString("address", address);
-        args.putString("wasteType", wasteType);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        fragmentId = getArguments().getInt("fragmentId", -1);
+    public InfoFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_info, container, false);
-        TextView addressTextView = view.findViewById(R.id.txtAddress);
-        TextView wasteTypeTxtView = view.findViewById(R.id.wasteTypeTxtView);
-        ImageButton btnDataDisplay = view.findViewById(R.id.btnMoreInfo);
+        addressTextView = view.findViewById(R.id.txtAddress);
 
-        btnDataDisplay.setOnClickListener(new View.OnClickListener() {
+        Button openInformations = view.findViewById(R.id.btnMoreInfo);
+
+        openInformations.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Bundle args = getArguments();
-                if (args != null) {
 
-                    /*String address = args.getString("address");
-                    addressTextView.setText(address);
+                String addressFromActivity = getArguments().getString("address");
+                String tagFromActivity = getArguments().getString("index");
+                String latitude = getArguments().getString("latitude");
+                String longitude = getArguments().getString("longitude");
 
-                    String wasteType = args.getString("wasteType");
-                    wasteTypeTxtView.setText(wasteType);*/
+                Intent i = new Intent(getActivity(), Data_display.class);
+                i.putExtra("address", addressFromActivity);
+                i.putExtra("index", tagFromActivity);
+                i.putExtra("latitude", latitude);
+                i.putExtra("longitude", longitude);
 
-                    String addressFromActivity = args.getString("address");
-                    String tagFromActivity = args.getString("index");
-                    String latitude = args.getString("latitude");
-                    String longitude = args.getString("longitude");
-
-
-                    Intent i = new Intent(getActivity(), Data_display.class);
-                    i.putExtra("address", addressFromActivity);
-                    i.putExtra("index", tagFromActivity);
-                    i.putExtra("latitude", latitude);
-                    i.putExtra("longitude", longitude);
-
-                    startActivity(i);
-                }
+                startActivity(i);
             }
         });
-
 
         return view;
     }
 
     public void updatePostalAddress(String addressText) {
-        if (txtAddress != null) {
-            txtAddress.setText(addressText);
+        if (addressTextView != null) {
+            Log.d("GetAddress", addressText);
+            addressTextView.setText(addressText);
         }
     }
+
 
 }
