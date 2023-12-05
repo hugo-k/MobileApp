87854d6f4a5ee82d68b5ad5101e65193b460a6d7
Hugo <hugo.kantardjian@edu.esiee.fr>
2023-12-05 16:44:27 +0100

    Change markers style

--- app/src/main/java/com/example/mobileapp/CustomClusterRenderer.java
+++ app/src/main/java/com/example/mobileapp/CustomClusterRenderer.java
@@ -1,6 +1,13 @@
 package com.example.mobileapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptor;
@@ -8,6 +15,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+import com.google.maps.android.ui.IconGenerator;
 
 import java.util.List;
 
@@ -22,9 +30,29 @@ public class CustomClusterRenderer extends DefaultClusterRenderer<WasteContainer
     @Override
     protected void onBeforeClusterItemRendered(WasteContainer item, MarkerOptions markerOptions) {
         float markerColor = determineMarkerColor(item);
-        BitmapDescriptor markerIcon = BitmapDescriptorFactory.defaultMarker(markerColor);
+        BitmapDescriptor markerIcon = getCustomMarker(markerColor);
         markerOptions.icon(markerIcon);
         super.onBeforeClusterItemRendered(item, markerOptions);
+    }
+
+    private BitmapDescriptor getCustomMarker(float color) {
+        int diameter = 50;
+        Bitmap bitmap = Bitmap.createBitmap(diameter, diameter, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        Paint circlePaint = new Paint();
+        circlePaint.setColor(Color.HSVToColor(new float[]{color, 1.0f, 1.0f}));
+        circlePaint.setAntiAlias(true);
+
+        canvas.drawCircle(diameter / 2, diameter / 2, diameter / 2, circlePaint);
+
+        Paint borderPaint = new Paint();
+        borderPaint.setColor(Color.BLACK);
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(5);
+
+        canvas.drawCircle(diameter / 2, diameter / 2, diameter / 2, borderPaint);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 
     private float determineMarkerColor(WasteContainer wasteContainer) {
